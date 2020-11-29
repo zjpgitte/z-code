7103eb351a0f1d2b52046c7f4f8bf676142afc5c
zjpgitte <slydian!.0>
2020-11-29 18:45:30 +0800

    2020_11_29:练习

--- 练习2020_11_28/练习2020_11_28/main.c
+++ 练习2020_11_28/练习2020_11_28/main.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 #include <windows.h>
-
+#pragma warning (disable:4996)
 int main()
 {
-	printf("%s\n", __FILE__);
+	int a, b;
+	char ch;
+	printf(" ‰»Î#\n");
+	scanf("%d", &a);//ch = getchar();
+	//printf(" ‰»Î#\n");
+	scanf("%d", &b);
+	printf("%d %d", a, b);
+	//printf("%c", ch);
+	/*printf("%s\n", __FILE__);
 	printf("%s\n", __LINE__);
 	printf("%s\n", __DATE__);
-	printf("%s\n", __TIME__);
+	printf("%s\n", __TIME__);*/
 
 	system("pause");
 	return 0;
