d7368de77c022b537afec55ca8e0cdb314ca296c
zjpgitte <1595873940@qq.com>
2021-01-16 18:51:58 +0800

    判断该树是否是镜像对称的

---
+++ leetcode-IsSymmetric/leetcode-IsSymmetric/main.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <windows.h>
+
+typedef struct BinaryTreeNode
+{
+	int val;
+	struct BinaryTreeNode *left;
+	struct BinaryTreeNode *right;
+}BinaryTreeNode;
+
+void BinaryTreeCreat(BinaryTreeNode **root, int **pa)
+{
+	if (**pa == -1)
+	{
+		(*pa)++;
+		return;
+	}
+	else
+	{
+		//创建结点
+		*root = (BinaryTreeNode *)malloc(sizeof(BinaryTreeNode));
+		(*root)->val = **pa;
+		(*root)->left = NULL;
+		(*root)->right = NULL;
+		(*pa)++;
+
+		//创建左右子树
+		BinaryTreeCreat(&(*root)->left, pa);
+		BinaryTreeCreat(&(*root)->right, pa);
+	}
+}
+
+void TravelJudge(BinaryTreeNode *p, BinaryTreeNode *q,int *tag)
+{
+	if (p == NULL && q == NULL)
+	{
+		return;
+	}
+	else if (p && !q || q && !p || p->val != q->val)
+	{
+		*tag = 1;
+		return;
+	}
+	else
+	{
+		TravelJudge(p->left, q->right, tag);
+		TravelJudge(p->right, q->left, tag);
+	}
+}
+
+int IsSymmetric(BinaryTreeNode *root)
+{
+	//要判断树root是否对称。
+	//只需判断 root 的左子树的左子树 和 root的右子树的右子树是否相等
+	//以及     root 的左子树的右子树 和 root的右子树的左子树是否相等
+
+	int tag = 0;
+
+	TravelJudge(root->left, root->right,&tag);
+
+	if (tag == 0)
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+int main()
+{
+	int arr[] = { 1, 2, 3, -1, -1, 4, -1, -1, 2, 4, -1, -1, 3, -1, -1 };
+	int *pa = arr;
+
+	//创建二叉树
+	BinaryTreeNode *root = NULL;
+	BinaryTreeCreat(&root, &pa);
+
+	printf("%d\n", IsSymmetric(root));
+	system("pause");
+	return 0;
+}
