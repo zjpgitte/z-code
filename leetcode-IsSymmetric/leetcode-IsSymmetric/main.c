971f27a387375b389249411b74643e1bce6efc29
zjpgitte <1595873940@qq.com>
2021-01-17 16:55:13 +0800

    判断一颗树是否镜像对称

--- leetcode-IsSymmetric/leetcode-IsSymmetric/main.c
+++ leetcode-IsSymmetric/leetcode-IsSymmetric/main.c
@@ -32,42 +32,79 @@ void BinaryTreeCreat(BinaryTreeNode **root, int **pa)
 	}
 }
 
-void TravelJudge(BinaryTreeNode *p, BinaryTreeNode *q,int *tag)
+//void TravelJudge(BinaryTreeNode *p, BinaryTreeNode *q,int *tag)
+//{
+//	if (p == NULL && q == NULL)
+//	{
+//		return;
+//	}
+//	else if (p && !q || q && !p || p->val != q->val)
+//	{
+//		*tag = 1;
+//		return;
+//	}
+//	else
+//	{
+//		TravelJudge(p->left, q->right, tag);
+//		TravelJudge(p->right, q->left, tag);
+//	}
+//}
+//
+//int IsSymmetric(BinaryTreeNode *root)
+//{
+//	//要判断树root是否对称。
+//	//只需判断 root 的左子树的左子树 和 root的右子树的右子树是否相等
+//	//以及     root 的左子树的右子树 和 root的右子树的左子树是否相等
+//
+//	int tag = 0;
+//
+//	TravelJudge(root->left, root->right,&tag);
+//
+//	if (tag == 0)
+//	{
+//		return 1;
+//	}
+//	else
+//	{
+//		return 0;
+//	}
+//}
+
+// p, q是对称位置上的节点
+int _isSymmetric(BinaryTreeNode* p, BinaryTreeNode* q)
 {
+	//结构相同值不相同
 	if (p == NULL && q == NULL)
-	{
-		return;
-	}
-	else if (p && !q || q && !p || p->val != q->val)
-	{
-		*tag = 1;
-		return;
-	}
-	else
-	{
-		TravelJudge(p->left, q->right, tag);
-		TravelJudge(p->right, q->left, tag);
-	}
-}
-
-int IsSymmetric(BinaryTreeNode *root)
-{
-	//要判断树root是否对称。
-	//只需判断 root 的左子树的左子树 和 root的右子树的右子树是否相等
-	//以及     root 的左子树的右子树 和 root的右子树的左子树是否相等
-
-	int tag = 0;
-
-	TravelJudge(root->left, root->right,&tag);
-
-	if (tag == 0)
 	{
 		return 1;
 	}
-	else
+	//结构不同
+	if (p && !q)
 	{
 		return 0;
 	}
+
+	if (q && !p)
+	{
+		return 0;
+	}
+	//结构相同值不相同
+	if (p->val != q->val)
+	{
+		return 0;
+	}
+
+	//结构相同值也相同，比较对称位置
+	return _isSymmetric(p->left, q->right) && _isSymmetric(p->right, q->left);
+}
+
+int IsSymmetric(BinaryTreeNode* root){
+	if (root == NULL)
+	{
+		return 1;
+	}
+
+	return _isSymmetric(root->left, root->right);
 }
 
 int main()
