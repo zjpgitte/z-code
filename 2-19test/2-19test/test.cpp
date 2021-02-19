c59504051fba3cc2d8ab00982797b6de97792a04
zjpgitte <1595873940@qq.com>
2021-02-19 15:45:47 +0800

    力扣练习

---
+++ 2-19test/2-19test/test.cpp
@@ -0,0 +1,14 @@
+#include "test.h"
+
+int main()
+{
+
+	/*char a = 0x7F;
+	printf("%d\n", a);
+	long b = 
+	cout << sizeof(long long) << endl;*/
+	long long b = 0x7FFFFFFF;
+	cout << b << endl;
+	system("pause");
+	return 0;
+}
